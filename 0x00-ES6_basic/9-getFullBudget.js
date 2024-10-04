d43e89a8e08215883bcbfdf4f63cf09d9941f214
Kelmfaker <kelmfaker@gmail.com>
2024-10-04 23:04:23 +0000

    getFullBudget

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,11 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
-      return `$${income}`;
+    getIncomeInDollars(inc) {
+      return `$${inc}`;
     },
-    getIncomeInEuros() {
-      return `${income} euros`;
+    getIncomeInEuros(inc) {
+      return `${inc} euros`;
     },
   };
 
