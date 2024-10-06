dc6ce07ceedd016727c3bca83b134d0c2cd5092a
Kelmfaker <kelmfaker@gmail.com>
2024-10-06 11:19:39 +0000

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,26 @@
 function handleResponseFromAPI(promise) {
-    promise
-      .then(() => {
-        console.log('Got a response from the API');
-        return {
-          status: 200,
-          body: 'success'
-        };
-      })
-      .catch(() => {
-        return new Error();
-      });
-  } 
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .then((response) => {
+      console.log('Got a response from the API');
+      return response;
+    })
+    .then((response) => {
+      console.log('Got a response from the API');
+      return response;
+    })
+    .catch((error) => {
+      console.error('API call failed');
+      return new Error(error);
+    });
+}
+
+// Simulating an error response with an Error object
+const promise = Promise.reject(new Error('API error'));
+handleResponseFromAPI(promise);
