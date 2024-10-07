1c14ad8b05cc50f485622bccb4b74e6b4dce69d2
Kelmfaker <kelmfaker@gmail.com>
2024-10-07 08:56:30 +0000

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,11 @@
-function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch((error) => {
-      console.error('Got an error from the API');
-      return new Error(error);
     });
 }
-
-export default handleResponseFromAPI;
