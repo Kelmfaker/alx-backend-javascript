161d68b1c2e4cd74f9793e430284ab00942224ea
Kelmfaker <kelmfaker@gmail.com>
2024-10-09 09:21:10 +0000

    update grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,17 +5,9 @@ export default function updateStudentGradeByCity(data, city, newGrades) {
 
   // Handling the Case When newGrades is Not Provided
   return dataLoc.map((student) => {
-    if (!newGrades) {
-      return {
-        ...student, grade: 'N/A',
-      };
-    }
-
-    // Updating Grades When newGrades is Provided
     const newGrade = newGrades.find((grade) => grade.studentId === student.id);
     return {
-      ...student,
-      grade: newGrade ? newGrade.grade : student.grade,
+      ...student, grade: newGrade ? newGrade.grade : 'N/A',
     };
   });
 }
