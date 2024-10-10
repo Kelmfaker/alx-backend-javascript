96ccbda7e063d8e876acf21d1d3df4359932046d
Kelmfaker <kelmfaker@gmail.com>
2024-10-10 00:40:02 +0000

    task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,26 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Index signature for additional properties
+}
+
+class TeacherClass implements Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+
+  constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, location: string, yearsOfExperience?: number) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullTimeEmployee = fullTimeEmployee;
+    this.location = location;
+    if (yearsOfExperience) {
+      this.yearsOfExperience = yearsOfExperience;
+    }
+  }
+}
