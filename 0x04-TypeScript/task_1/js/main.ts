ef3305357102f646ab66be302a66f2acee307ced
Kelmfaker <kelmfaker@gmail.com>
2024-10-13 17:06:09 +0000

    updates

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,19 @@ interface Teacher {
   [key: string]: any; // Index signature for additional properties
 }
 
+interface Directors extends Teacher {
+ numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string{
+return `${firstName[0]} ${lastName}`;
+}
+
+interface function printTeacherFunction(){
+(firstName: string, lastName: string): string;
+}
+
+
 class TeacherClass implements Teacher {
   readonly firstName: string;
   readonly lastName: string;
