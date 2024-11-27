90499dcc9f47323d69d276c031bafe4b12aaa934
Kelmfaker <kelmfaker@gmail.com>
2024-11-27 08:07:36 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,8 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString();
   process.stdout.write(`Your name is: ${name}\n`);
-});
-process.on('exit', () => {
-  // eslint-disable-next-line no-console
-  console.log('This important software is now closing');
+  if (!process.stdin.isTTY) {
+    // eslint-disable-next-line no-console
+    console.log('This important software is now closing');
+  }
+  process.exit();
 });
