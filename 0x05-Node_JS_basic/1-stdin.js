6ee2d5a97e43fb1a1d6db99c83d0a96748f5311a
Kelmfaker <kelmfaker@gmail.com>
2024-11-27 07:52:00 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,8 @@
 // stdin
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+  const name = data.toString();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
 });
 process.on('exit', () => {
   // eslint-disable-next-line no-console
