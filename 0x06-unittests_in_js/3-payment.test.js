aebfeb71cf94ce00bd0826c00b9e993b8c4f73dc
Kelmfaker <kelmfaker@gmail.com>
2024-11-29 22:02:18 +0000

    Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with SUM, 100, and 20', function() {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    spy.restore();
+  });
+});
