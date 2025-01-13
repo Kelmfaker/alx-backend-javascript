1a20fa653dccc165b51008afc59382828f60d98a
Kelmfaker <kelmfaker@gmail.com>
2025-01-13 19:34:56 +0000

    http express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+const express = require('express');
+const readfile = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+// Define the path to the file
+const filePath = path.join(__dirname, 'database.csv');
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (res, req) => {
+  res.send('This is the list of our students', readfile(filePath));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+module.exports = app;
