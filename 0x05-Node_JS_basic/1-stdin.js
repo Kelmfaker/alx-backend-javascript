299cc673db0f725b3319f1ec67e2a6322a6cf0be
Kelmfaker <kelmfaker@gmail.com>
2025-01-13 15:06:09 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	  const InPut = process.stdin.read();
-
-	  if (InPut) {
-		      process.stdout.write(`Your name is: ${InPut}`);
-		    }
+  const InPut = process.stdin.read();
+  if (InPut) {
+    process.stdout.write(`Your name is: ${InPut}`);
+  }
 });
 
 process.stdin.on('end', () => {
-	  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
